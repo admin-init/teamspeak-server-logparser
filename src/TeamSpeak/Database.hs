39a5f1dcb421f07dcb8bdb175709d36ae137bec3
admininit <rainfallxx@163.com>
2025-12-25 17:50:37 +0800

    feat(database): add core database connection handling
    - Create  module
    - Add  function to establish SQLite connection
    - Add  function to close the connection

--- src/TeamSpeak/Database.hs
+++ src/TeamSpeak/Database.hs
@@ -1,2 +1,19 @@
--- src/TeamSpeak/Database.hs
-module TeamSpeak.Database where
+-- File: src/TeamSpeak/Database.hs
+
+-- | Module for handling SQLite database connections.
+module TeamSpeak.Database
+    ( Connection -- Export the Connection type from sqlite-simple
+    , openConnection
+    , closeConnection
+    ) where
+
+import Database.SQLite.Simple (Connection, open, close)
+
+-- | Opens a connection to an SQLite database file.
+--   The file will be created if it does not exist.
+openConnection :: FilePath -> IO Connection
+openConnection = open
+
+-- | Closes the given SQLite database connection.
+closeConnection :: Connection -> IO ()
+closeConnection = close
